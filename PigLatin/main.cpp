643847e8d67e55e876f7756ce5834d574d5b9a6b
mdasadul <mdasadul@ualberta.ca>
2015-06-10 23:38:44 -0600

    adding input and output file and updating documentation

--- PigLatin/main.cpp
+++ PigLatin/main.cpp
@@ -1,3 +1,18 @@
+/*
+*
+*PigLatin is a Language Game in which English word are altered.
+* For more information please have a look on wiki(http://en.wikipedia.org/wiki/Pig_Latin)
+*
+* In this program I implemented two rules
+* 1)For words that begin with consonant sounds,
+*  the initial consonant or consonant cluster is
+*  moved to the end of the word, and "ay"  is added,
+   as in the following examples:
+   "pig" → "igpay"
+* 2) For words which begin with vowel sounds or silent letter, one just adds "yay" to the end. Examples are:
+    "eat" → "eatyay"
+* @author Md Asadul Islam Date: 11/06/2015
+*/
 
 #include <iostream>
 #include<stdio.h>
@@ -11,9 +26,12 @@ class PigLatin
     bool isvowel(char);
 public:
     void getText();
-    void convertToPigLation();
+    string convertToPigLation(string);
     void displayText();
 };
+
+
+
 bool PigLatin::isvowel( char c)
 {
     c=tolower(c);
@@ -27,61 +45,71 @@ bool PigLatin::isvowel( char c)
     }
 }
 
+
+
 void PigLatin::getText()
 {
     freopen("inputText.txt","r",stdin);
-    freopen("outputText.txt","w",stdout);
+
     char tempInput;
-    int i;
     string originalWord,pigLatinWord;
+
     while(scanf("%c",&tempInput)!=EOF)
     {
-
-        //printf("%c",tempInput);
         if(isalpha(tempInput))
-           {
-                originalWord += tempInput;
-           }
-           else
-           {
-               if(isvowel(originalWord[0]))
-               {
-                    pigLatinWord = originalWord+"yay";
-               }
-               else
-               {
-                    for(i=0;i<originalWord.length();i++)
-                    {
-
-                        if(isvowel(originalWord[i]))
-                        {
-                            pigLatinWord = originalWord.substr(i);
-                            pigLatinWord += originalWord.substr(0,i)+"ay";
-                            break;
-                        }
-                    }
-
-               }
-                outputText += pigLatinWord +tempInput;
-                pigLatinWord = "";
-                originalWord = "";
-           }
+        {
+            originalWord += tempInput;
+        }
+        else
+        {
+            pigLatinWord = convertToPigLation(originalWord);
+            outputText += pigLatinWord +tempInput;
+            pigLatinWord = "";
+            originalWord = "";
+        }
     }
-    printf("%s",outputText.c_str());
-}
-void PigLatin::convertToPigLation()
-{
 
 }
+
+
+
+string PigLatin::convertToPigLation(string originalWord)
+{
+    string wordInPigLatin="";
+    signed int i;
+    if(isvowel(originalWord[0]))
+    {
+        wordInPigLatin = originalWord+"yay";
+    }
+    else
+    {
+        for(i=0;i<originalWord.length();i++)
+        {
+            if(isvowel(originalWord[i]))
+            {
+                wordInPigLatin = originalWord.substr(i);
+                wordInPigLatin += originalWord.substr(0,i)+"ay";
+                break;
+            }
+        }
+    }
+    return wordInPigLatin;
+}
+
+
+
 void PigLatin::displayText()
 {
-
-
+    freopen("outputText.txt","w",stdout);
+    printf("%s",outputText.c_str());
 }
+
+
 
 int main()
 {
     PigLatin pigLationObj;
     pigLationObj.getText();
+    pigLationObj.displayText();
     return 0;
 }
